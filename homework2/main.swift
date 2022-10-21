1c073068d3919d6388f6018902c962f15bd6af4f
XVIIKGZ <nazar.kydyraliev@gmail.com>
2022-10-21 23:26:14 +0600

    готовый вариант

--- homework2/main.swift
+++ homework2/main.swift
@@ -14,36 +14,75 @@ import Foundation
 //При вводе одного из городов в readLine (Бишкек, Чолпон-Ата, Талас, Ош, Джалал-Абад, Нарын, Баткен) выводить в итоге данные о погоде на ближайшие 3 дня в указанном городе
 //
 print("Прогноз погоды\nВведите город:")
+
 let weather = readLine()!
-var bishkek = "Бишкек"
-var cholpon = "Чолпон-Ата"
-var talas = "Талас"
-var osh = "Ош"
-var djlal = "Джалал-Абад"
-var naryn = "Нарын"
-var batken = "Баткен"
+    let bishkek = "Бишкек"
+    let cholpon = "ЧолпонАта"
+    let talas = "Талас"
+    let osh = "Ош"
+    let djlal = "ДжалалАбад"
+    let naryn = "Нарын"
+    let batken = "Баткен"
 func weatherKg(){
     if weather == bishkek{
-        print("\(bishkek), погода на три дня:\nСегодня - облачно\nЗавтра - пасмурно\nПослезавтра - ясно")}
-    if weather == cholpon{
-        print("\(cholpon), погода на три дня:\nСегодня - ясно\nЗавтра - переменная облачность\nПослезавтра - ясно")
+        print("\n\(bishkek), погода на три дня:\n\nСегодня - облачно\nЗавтра - пасмурно\nПослезавтра - ясно")
+    }else if weather == cholpon{
+        print("\n\(cholpon), погода на три дня:\n\nСегодня - ясно\nЗавтра - переменная облачность\nПослезавтра - ясно")
     }else if weather == talas{
-        print("\(talas), погода на три дня:\nСегодня - ясно\nЗавтра - ясно\nПослезавтра - ясно")
+        print("\n\(talas), погода на три дня:\n\nСегодня - ясно\nЗавтра - ясно\nПослезавтра - ясно")
     }else if weather == osh{
-        print("\(osh), погода на три дня:\nСегодня - переменная облачность\nЗавтра - ясно\nПослезавтра - облачно")
+        print("\n\(osh), погода на три дня:\n\nСегодня - переменная облачность\nЗавтра - ясно\nПослезавтра - облачно")
     }else if weather == djlal{
-        print("\(djlal),погода на три дня:\nСегодня - облачно\nЗавтра - пасмурно\nПослезавтра - облачно")
+        print("\n\(djlal),погода на три дня:\n\nСегодня - облачно\nЗавтра - пасмурно\nПослезавтра - облачно")
     }else if weather == naryn{
-        print("\(naryn), погода на три дня:\nСегодня - дождь\nЗавтра - дождь\nПослезавтра - облачно")
+        print("\n\(naryn), погода на три дня:\n\nСегодня - дождь\nЗавтра - дождь\nПослезавтра - облачно")
     }else if weather == batken{
-        print("\(batken) погода на три дня:\nСегодня - ясно\nЗавтра - дождь\nПослезавтра - пасмурно")
+        print("\n\(batken) погода на три дня:\n\nСегодня - ясно\nЗавтра - дождь\nПослезавтра - пасмурно")
     }else{
         print("Ошибка ввода города")
     }
 }
+weatherKg()
+
 
 //№2. Используя условные операторы и функции создать программу, которая после ввода 5 наименований продуктов через readLine будет считать итоговую стоимость покупок (со скидкой в 5%)
+
+let cola = ("cola")
+let energy = ("energy")
+let cigarette = ("cigarette")
+let twix = ("twix")
+let gum = ("gum")
+func productPrice(){
+print("\nУ нас есть только 5 товаров на твой вкус: \(cola), \(energy), \(cigarette), \(twix), \(gum)")
+let colaPr = 100.00
+    let energyPr = 88.00
+    let cigarettePr = 112.00
+    let twixPr = 44.00
+    let gumPr = 23.00
+    let summa = colaPr + energyPr + cigarettePr + twixPr + gumPr
+    let skidka = summa * 0.05
+    let summaWithSkidka = summa - skidka
+print("Выберите 5 товаров:")
+let oneProduct = readLine()!
+print("Ещё 4")
+let twoProduct = readLine()!
+print("Ещё 3")
+let threeProduct = readLine()!
+print("Ещё 2")
+let fourProduct = readLine()!
+print("Ещё 1")
+let fiveProduct = readLine()!
+    if oneProduct == cola && twoProduct == energy && threeProduct == cigarette && fourProduct == twix && fiveProduct == gum{
+        print("\nВаши товары:\n\n\(cola) - \(colaPr) som\n\(energy) - \(energyPr) som\n\(cigarette) - \(cigarettePr) som\n\(twix) - \(twixPr) som\n\(gum) - \(gumPr) som")
+        print("\nСумма: \(summa) som\nСкидка: \(skidka) som\nСумма с учётом скидки: \(summaWithSkidka) som")
+    }else{
+        print("Попробуйте ещё раз")
+    }
+}
+productPrice()
+
 //
+
 //В итоге должно быть так: (например)
 //Ваши товары:
 //Кола - 50с
@@ -57,3 +96,30 @@ func weatherKg(){
 //
 //№3. Доработать калькулятор с прошлого урока с помощью условных операторов и соединить всё с readLine (например сначала спрашиваете число, после чего “Какое действие совершить?”, затем второе число и т.д ) (максимум доступно 5 действий). И в итоге выдать итоговый результат используя заранее подготовленные функции для вычислений
 
+print("\nКалькулятор")
+func calculator(){
+print("Введите цифру")
+let numcalone: String = readLine()!
+print("Введите действие (+ - * /)")
+var sumcal: String = readLine()!
+print("Введите вторую цифру")
+let numcaltwo: String = readLine()!
+var summa = "+"
+var minus = "-"
+var umnoj = "*"
+var delit = "/"
+if sumcal == summa {
+    print("\(numcalone) \(sumcal) \(numcaltwo) =",(Int(numcalone) ?? 0) + (Int(numcaltwo) ?? 0))
+}
+if sumcal == minus {
+    print("\(numcalone) \(sumcal) \(numcaltwo) =",(Int(numcalone) ?? 0) - (Int(numcaltwo) ?? 0))
+    
+}
+if sumcal == umnoj {
+    print("\(numcalone) \(sumcal) \(numcaltwo) =",(Int(numcalone) ?? 0) * (Int(numcaltwo) ?? 0))
+}
+if sumcal == delit {
+    print("\(numcalone) \(sumcal) \(numcaltwo) =",(Int(numcalone) ?? 0) / (Int(numcaltwo) ?? 0))
+}
+}
+calculator()
